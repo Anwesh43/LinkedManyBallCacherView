03948cc1d74902f31025597afd9bfdf5e4743889
Anwesh43 <anweshthecool0@gmail.com>
2021-05-23 02:23:43 +0530

    adding Canvas extension methods

--- manyballcatcherview/src/main/java/com/example/manyballcatcherview/ManyBallCatcherView.kt
+++ manyballcatcherview/src/main/java/com/example/manyballcatcherview/ManyBallCatcherView.kt
@@ -29,3 +29,38 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i  * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawManyBallCatcher(scale : Float, w : Float, h : Float, paint : Paint) {
+    val r : Float = w / (4 * balls + 2)
+    val size : Float = w / 2
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-w / 2, 0f)
+        rotate(180f * sf2)
+        drawLine(0f, 0f, -size, 0f, paint)
+        restore()
+    }
+    for (j in 0..(balls - 1)) {
+        val sfj : Float = sf.divideScale(2 + j, parts)
+        save()
+        translate(-w / 2 + r + 3 * r * j, -h / 2 + r + (h  / 2 - 2 * r) * sfj)
+        drawCircle(0f, 0f, r * sf1, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawMBCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawManyBallCatcher(scale, w, h, paint)
+}
